fc6c2bef1356cec04507c83107303a3e0114b62d
carlos alberto <karlozz157@karlozz157.(none)>
2014-04-03 13:23:41 -0600

    styles in search products

--- application/views/site/search.php
+++ application/views/site/search.php
@@ -1,7 +1,8 @@
-<?php foreach ($products as $product): ?>
-	<div class="content-images-product-category"><a href="<?php echo base_url(); ?>site/product/<?php echo $product['id']; ?>">
-		<?php $image = substr($product['image'], 0, strrpos($product['image'], '.')).'_200x200'.substr($product['image'], strrpos($product['image'], '.')); ?>
-		<img src="<?php echo base_url(); ?>assets/media/img/200x200/<?php echo $image; ?>" class="images-product-category">	
+<?php foreach ($products as $i => $product): ?>
+	<div class="content-images-product-category-search" style="<?php echo ($i%5 == 0) ? 'margin: 0;' : '' ?>">
+		<a href="<?php echo base_url(); ?>site/product/<?php echo $product['id']; ?>">
+		<?php $image = substr($product['image'], 0, strrpos($product['image'], '.')).'_180x180'.substr($product['image'], strrpos($product['image'], '.')); ?>
+		<img src="<?php echo base_url(); ?>assets/media/img/180x180/<?php echo $image; ?>" class="images-product-category">	
 		<span class="description-product-category">
 			<?php echo strtoupper($product['name']); ?>
 			<span>
