04e770c0996c941a28377ef580d13976b7036ab0
carlos alberto <karlozz157@karlozz157.(none)>
2014-04-04 17:44:17 -0500

    border 1px slider promociones

--- application/views/site/index.php
+++ application/views/site/index.php
@@ -14,7 +14,7 @@
 
 <?php if($promociones): ?>
 <div id="slider2_container" class="slider-index" style="position: relative; top: 0px; left: 0px; height: 512px; width: 1020px;">
-    <div u="slides" style="border: 3px solid #73787C; cursor: move; position: absolute; overflow: hidden; left: 0px; top: 0px; 	height: 512px; width: 1020px;">
+    <div u="slides" style="border: 1px solid #73787C; cursor: move; position: absolute; overflow: hidden; left: 0px; top: 0px; 	height: 512px; width: 1020px;">
 		<?php foreach ($promociones as $promocion): ?>
 			<div><a href="<?php echo '' != $slider['url']?$slider['url']:'#'; ?>"><img src="<?php echo base_url(); ?>assets/media/img/<?php echo $promocion['image']; ?>" class="slider"></a></div>
 		<?php endforeach ?>
