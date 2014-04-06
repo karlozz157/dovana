a8f4cd8fb873c41f043850a056d96c99d9b43e45
carlos alberto <karlozz157@karlozz157.(none)>
2014-04-06 12:14:27 -0500

    styles home slider

--- application/views/site/index.php
+++ application/views/site/index.php
@@ -1,23 +1,27 @@
 <img src="<?php echo base_url(); ?>assets/site/images/title-index.png" style="margin: auto; display: block; width: 600px">
 
 <?php if($sliders): ?>
-<div id="slider1_container" class="slider-index" style="position: relative; top: 0px; left: 0px; height: 512px; width: 1020px;">
-    <div u="slides" style="border: 1px solid #73787C; cursor: move; position: absolute; overflow: hidden; left: 0px; top: 0px; 	height: 512px; width: 1020px;">
-		<?php foreach ($sliders as $slider): ?>
-			<div><a href="<?php echo '' != $slider['url']?$slider['url']:'#'; ?>"><img src="<?php echo base_url(); ?>assets/media/img/<?php echo $slider['image']; ?>" class="slider"></a></div>
-		<?php endforeach ?>
-    </div>
+<div style="border: 1px solid #73787C; padding: 10px;">
+	<div id="slider1_container" class="slider-index" style="position: relative; top: 0px; left: 0px; height: 492px; width: 1000px;">
+	    <div u="slides" style="cursor: move; text-align: center; position: absolute; overflow: hidden; left: 0px; top: 0px; 	height: 492px; width: 1000px;">
+			<?php foreach ($sliders as $slider): ?>
+				<div><a href="<?php echo '' != $slider['url']?$slider['url']:'#'; ?>"><img src="<?php echo base_url(); ?>assets/media/img/<?php echo $slider['image']; ?>" class="slider"></a></div>
+			<?php endforeach ?>
+	    </div>
+	</div>
 </div>
 <?php endif; ?>
 
 <h3 id="title-promociones">NOTICIAS DOVANA // PROMOCIONES</h3>
 
 <?php if($promociones): ?>
-<div id="slider2_container" class="slider-index" style="position: relative; top: 0px; left: 0px; height: 512px; width: 1020px;">
-    <div u="slides" style="border: 1px solid #73787C; cursor: move; position: absolute; overflow: hidden; left: 0px; top: 0px; 	height: 512px; width: 1020px;">
-		<?php foreach ($promociones as $promocion): ?>
-			<div><a href="<?php echo '' != $slider['url']?$slider['url']:'#'; ?>"><img src="<?php echo base_url(); ?>assets/media/img/<?php echo $promocion['image']; ?>" class="slider"></a></div>
-		<?php endforeach ?>
-    </div>
+<div style="border: 1px solid #73787C; padding: 10px;">
+	<div id="slider2_container" class="slider-index" style="position: relative; top: 0px; left: 0px; height: 492px; width: 1000px;">
+	    <div u="slides" style="cursor: move; position: absolute; overflow: hidden; left: 0px; top: 0px; 	height: 492px; width: 1000px;">
+			<?php foreach ($promociones as $promocion): ?>
+				<div><a href="<?php echo '' != $slider['url']?$slider['url']:'#'; ?>"><img src="<?php echo base_url(); ?>assets/media/img/<?php echo $promocion['image']; ?>" class="slider"></a></div>
+			<?php endforeach ?>
+	    </div>
+	</div>
 </div>
 <?php endif; ?>
