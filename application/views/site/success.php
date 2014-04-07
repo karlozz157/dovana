5aa7d93c19ef00ba09e70af87aecbe68bb39c629
carlos alberto <karlozz157@karlozz157.(none)>
2014-04-06 22:26:34 -0500

    styles in checkout success and put the logo image in the table

--- application/views/site/success.php
+++ application/views/site/success.php
@@ -1,83 +1,14 @@
-<style type="text/css">
-    #container-table
-    {
-        background: #fff;
-        font-family: 'Brandon Grotesque Medium';
-        padding: 25px 10px;
-        margin-bottom: 25px;
-    }
-    #table-details-purchase
-    {
-        margin: auto;
-        width: 700px;
-    }
-
-    .text
-    {
-        font-size: 12pt;
-    }
-
-    .text p
-    {  
-        margin: 10px 0;
-    }
-
-    .details
-    {
-        width: 700px;
-    }
-
-    .details thead
-    {
-        background-color: rgba(115, 120, 124, .2);
-    }
-
-    .details thead th
-    {
-        font-family: 'Brandon Grotesque Medium'; 
-        font-size: 9pt;
-        font-weight: normal;
-        text-transform: uppercase;
-        padding: 7px 0px;
-    }
-
-
-    .details tbody td
-    {
-        font-size: 10pt;
-        font-family: 'Brandon Grotesque Bold';  
-        text-transform: uppercase;     
-        padding: 7px 14px;
-    }
-
-    .details-send td
-    {
-        font-family: 'Brandon Grotesque';
-        font-size: 10pt;
-        font-style: italic;
-    }
-
-    .column-center
-    {  
-        text-align: center;
-    }
-
-    #list-product
-    {
-        margin-bottom: 20px;
-    }
-    </style>
-<div id="container-table">
-    <table id="table-details-purchase">
-        <tr><td style="font-size: 26pt; font-style: italic; text-transform: uppercase;"><p>Hola <span><?php echo ucwords($user['name']); ?></span></p></td></tr>
+<div id="container-table-purchase-success">
+    <table id="details-purchase-success">
+        <tr><td id="welcome-details-purchase-success">Hola <?php echo ucwords($user['name']); ?></td></tr>
         <tr><td>&nbsp;</td></tr>
         <tr>
-            <td style="font-style: 14pt; font-style: italic; font-family: 'Brandon Grotesque';"><p>A CONTINUACIÓN ENCONTRARÁS LOS DETALLES DE TU COMPRA...</p></td>
+            <td>A CONTINUACIÓN ENCONTRARÁS LOS DETALLES DE TU COMPRA...</td>
         </tr>
         <tr><td>&nbsp;</td></tr>
         <tr>
-            <td style="border-top: 1px solid #333F48; border-bottom: 1px solid #333F48; padding: 15px 0 0 0;">    
-                <table id="list-product" class="details">
+            <td id="head-products-details-purchase-success">    
+                <table id="list-product-details-purchase-success" class="details-purchase-success">
                     <thead>
                         <th>Producto</th>
                         <th>Cantidad</th>
@@ -88,9 +19,9 @@
                         <?php foreach ($products as $product): ?>
                             <tr>
                                 <td><?php echo $product['name']; ?></td>
-                                <td class="column-center"><?php echo $product['quantity']; ?></td>
-                                <td class="column-center">$<?php echo $product['unit_price']; ?></td>
-                                <td class="column-center">$<?php echo $product['total_price']; ?></td>
+                                <td class="column-center-details-purchase-success"><?php echo $product['quantity']; ?></td>
+                                <td class="column-center-details-purchase-success">$<?php echo $product['unit_price']; ?></td>
+                                <td class="column-center-details-purchase-success">$<?php echo $product['total_price']; ?></td>
                             </tr>
                         <?php endforeach; ?>
                     </tbody>
@@ -98,7 +29,7 @@
             </td>
         </tr>
         <tr>
-            <td style="padding-top: 15px;">
+            <td style="background: url('<?php echo base_url(); ?>/assets/site/images/logo-pajarito.png') no-repeat; background-position: center;  padding-top: 15px;">
                 <table class="details-send">
                     <tbody>
                         <tr><td style="font-style: italic; font-size: 11pt;">DIRECCIÓN DE ENVÍO</td></tr>
@@ -110,22 +41,21 @@
                         <tr><td><?php echo $address['calle']; ?> - <?php echo $address['no_exterior']; ?></td></tr>
                     </tbody>
                 </table>
-            </td>
-        </tr>
-        <tr><td>&nbsp;</td></tr>
-        <tr>
-            <td>
+                <table>
+                    <tr><td>&nbsp;</td></tr>
+                    <tr><td>&nbsp;</td></tr>
+                </table>
                 <table class="details-send">
                     <tbody>
                         <tr><td style="font-style: italic; font-size: 11pt;">ACUSE DE RECIBO</td></tr>
                         <tr><td>&nbsp;</td></tr>
-                        <tr><td>De: <?php echo $message['from_name']; ?></td></tr>
-                        <tr><td>Para: <?php echo $message['to_name']; ?></td></tr>
+                        <tr><td>De: <?php echo strtoupper($message['from_name']); ?></td></tr>
+                        <tr><td>Para: <?php echo strtoupper($message['to_name']); ?></td></tr>
                     </tbody>
                 </table>
             </td>
         </tr>
-        <tr><td>&nbsp;</td></tr>
+        <tr><td style="text-align: right;">En breve te enviaremos tu factura a tu mail.</td></tr>
         <tr><td>&nbsp;</td></tr>
         <tr>
             <td style="text-align: center; border-top: 1px solid #333F48; font-size: 26pt; font-style: italic; padding-top: 30px;">
